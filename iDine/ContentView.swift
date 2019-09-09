bfd98709903b2a34ced116627ac9630507cc4854
Alex Paul <alexpaulnyc@gmail.com>
2019-09-08 21:55:22 -0400

    format item name font to headline

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
       Image(item.thumbnailImage)
       VStack(alignment: .leading) {
         Text(item.name)
+          .font(.headline)
         Text("$\(item.price)")
       }
     }
