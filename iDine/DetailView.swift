59087550a9a2d6856151b5973d04447f5ea144a5
Alex Paul <alexpaulnyc@gmail.com>
2019-09-08 21:59:58 -0400

    created a DetailView

---
+++ iDine/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  iDine
+//
+//  Created by Alex Paul on 9/8/19.
+//  Copyright © 2019 Hacking with Swift. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
