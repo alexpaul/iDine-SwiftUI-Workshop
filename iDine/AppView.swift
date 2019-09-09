cf1ee05f75c6cb0fdb87e5fd389be1286aa44c10
Alex Paul <alexpaulnyc@gmail.com>
2019-09-08 23:01:44 -0400

    created a AppView and added a TabView to it to be the main entry point of the app

---
+++ iDine/AppView.swift
@@ -0,0 +1,27 @@
+//
+//  AppView.swift
+//  iDine
+//
+//  Created by Alex Paul on 9/8/19.
+//  Copyright © 2019 Hacking with Swift. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+  var body: some View {
+    TabView {
+      ContentView()
+        .tabItem {
+          Image(systemName: "1.circle")
+          Text("Menu")
+      }
+    }
+  }
+}
+
+struct AppView_Previews: PreviewProvider {
+  static var previews: some View {
+    AppView()
+  }
+}
